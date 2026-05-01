f02b23ab4302dff6d2b45d929fda369a5f54b804
yash00gandhi-lgtm <yash00gandhi@gmail.com>
2026-05-01 21:18:08 +0530

    fix: static crash + render boot fix

--- core/settings.py
+++ core/settings.py
@@ -132,9 +132,7 @@ USE_TZ = True
 # ========================
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
